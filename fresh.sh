7e57d3237ed955e832915bb2bb95af86ac5ae476
Michael Bernhardt <neomagic100@gmail.com>
2024-07-13 18:03:51 -0400

    guards

--- fresh.sh
+++ fresh.sh
@@ -23,8 +23,18 @@ editConf () {
 	if ! [ -f /etc/sysctl.d/99-sysctl.conf ]; then
 		touch /etc/sysctl.d/99-sysctl.conf
 	fi
-	echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/99-sysctl.conf
-	echo "net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.d/99-sysctl.conf
+
+	IPV4=$(egrep ^net.ipv4.ip_forward < /etc/sysctl.d/99-sysctl.conf)
+	IPV6=$(egrep ^net.ipv6.conf.all.forwarding < /etc/sysctl.d/99-sysctl.conf)
+
+	if [[ $IPV4 == "" ]]; then
+		echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/99-sysctl.conf
+	fi
+
+	if [[ $IPV6 == "" ]]; then
+		echo "net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.d/99-sysctl.conf
+	fi
+	
 	sudo sysctl -p
 }
 
@@ -38,7 +48,17 @@ installApt () {
 
 enableSSH () {
 	systemctl enable ssh 2>&1 3>&1 1>log.txt 2>/dev/null
-	echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+	if ! [ -f /etc/ssh/sshd_config ]; then
+		touch /etc/sysctl.d/99-sysctl.conf
+	fi
+
+	LOGIN_PERMITTED=$(egrep ^"PermitRootLogin yes" < /etc/ssh/sshd_config)
+	
+	if [[ $LOGIN_PERMITTED == "" ]]; then
+		echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+		systemctl restart sshd
+	fi
+
 }
 
 runMainMenu () {
@@ -96,6 +116,8 @@ main () {
 	fi
 
 	clear
+	echo "Completed scripts..."
+	echo
 }
 
 main
